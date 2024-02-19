53ad802ea215e478017ea03f7bf4b3566d50f94d
Alexey Kerpel <alexeykerpel15@gmail.com>
2024-02-19 20:30:09 +0000

    Got the lines

--- lua/find-next/init.lua
+++ lua/find-next/init.lua
@@ -1,20 +1,42 @@
 local M = {}
 
-function M.get_branch_name()
-	for line in io.popen("git branch 2>nul"):lines() do
-		local m = line:match("%* (.+)$")
-		if m then
-			return m
-		end
+-- function M.get_branch_name()
+-- 	for line in io.popen("git branch 2>nul"):lines() do
+-- 		local m = line:match("%* (.*)$")
+-- 		if m then
+-- 			return m
+-- 		end
+-- 	end
+--
+-- 	return false
+-- end
+function M.get_text(input)
+	local handle = io.popen(input)
+	local result
+
+	if handle then
+		result = handle:read("*a")
+		handle:close()
 	end
 
-	return false
+	return result
 end
-
+function M.filter_lines(input_text)
+	local pattern = "^00000000"
+	local lines = {}
+	for line in input_text:gmatch("[^\n]+") do
+		if line:find(pattern) then
+			table.insert(lines, line)
+		end
+	end
+	return table.concat(lines, "\n")
+end
 function M.setup()
-	print("Hello, World!")
-	print("Hello!")
-	print(M.get_branch_name())
+	-- print(M.get_branch_name())
+	print("----------")
+
+	local changes = M.get_text("git blame ./lua/find-next/init.lua")
+	print(M.filter_lines(changes))
 end
 
 M.setup()

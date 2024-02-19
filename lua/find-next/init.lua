104936ae375ee30bbfb161815815392841e66691
Alexey Kerpel <alexeykerpel15@gmail.com>
2024-02-19 21:23:06 +0000

    Moving pointer, added events

--- lua/find-next/init.lua
+++ lua/find-next/init.lua
@@ -1,15 +1,5 @@
 local M = {}
 
--- function M.get_branch_name()
--- 	for line in io.popen("git branch 2>nul"):lines() do
--- 		local m = line:match("%* (.*)$")
--- 		if m then
--- 			return m
--- 		end
--- 	end
---
--- 	return false
--- end
 function M.get_text(input)
 	local handle = io.popen(input)
 	local result
@@ -21,6 +11,7 @@ function M.get_text(input)
 
 	return result
 end
+
 function M.filter_lines(input_text)
 	local pattern = "^00000000"
 	local lines = {}
@@ -31,12 +22,62 @@ function M.filter_lines(input_text)
 	end
 	return table.concat(lines, "\n")
 end
+
+function M.get_blame()
+	local current_line = vim.fn.line(".")
+	local current_file = vim.fn.expand("%:p")
+	local current_buffer = vim.fn.bufname()
+
+	local max_line = vim.fn.line("$")
+	print(current_line)
+	print(current_file)
+	print(max_line)
+
+	print(current_buffer)
+	local git_blame = M.get_text("git blame " .. current_file)
+
+	return M.filter_lines(git_blame)
+end
+
+-- TODO: Add error handling
+-- TODO: Probably we need to fire git blame on some event (file save)
+-- TODO: Add debounce so we don't run this too much
+
+local events = {
+	"BufWritePost",
+	"BufEnter",
+}
+
+function M.print_hi()
+	print("hi")
+end
+
+function M.listen_to_events()
+	for _, event in ipairs(events) do
+		vim.cmd("autocmd " .. event .. " * lua require('find-next').print_hi()")
+	end
+end
+
+function M.move_pointer(line_number)
+	vim.fn.cursor({ line_number, 0 })
+end
+
 function M.setup()
 	-- print(M.get_branch_name())
 	print("----------")
 
-	local changes = M.get_text("git blame ./lua/find-next/init.lua")
-	print(M.filter_lines(changes))
+	M.listen_to_events()
+	-- local changes = M.get_text("git blame ./lua/find-next/init.lua")
+	-- print(M.filter_lines(changes))
+
+	vim.keymap.set("n", "gBa", function()
+		print(M.get_blame())
+		M.move_pointer(7)
+	end)
+	vim.keymap.set("n", "gBw", function()
+		print(vim.fn.line("."))
+	end)
+	-- print(M.get_blame())
 end
 
 M.setup()

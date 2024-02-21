cb6ca75d29f0187b7e15f7871c5960ab28dd2748
Alexey Kerpel <alexeykerpel15@gmail.com>
2024-02-21 19:39:50 +0000

    Somewhat working

--- lua/find-next/init.lua
+++ lua/find-next/init.lua
@@ -24,12 +24,13 @@ function M.filter_lines(input_text)
 end
 
 function M.get_blame()
+	local x = {}
 	local current_line = vim.fn.line(".")
 	local current_file = vim.fn.expand("%:p")
 	local current_buffer = vim.fn.bufname()
 
 	local max_line = vim.fn.line("$")
-	print(current_line)
+	print(current_line .. "asd")
 	print(current_file)
 	print(max_line)
 
@@ -45,16 +46,44 @@ end
 
 local events = {
 	"BufWritePost",
-	"BufEnter",
+	-- "BufEnter",
 }
 
-function M.print_hi()
-	print("hi")
+local changedLines = {}
+
+function M.update_lines()
+	print(M.blame_to_table(M.get_blame()))
+
+	for i, _ in ipairs(changedLines) do
+		table.remove(changedLines, i)
+	end
+
+	for _, value in ipairs(M.blame_to_table(M.get_blame())) do
+		-- convert "number)" to number
+		table.insert(changedLines, value)
+	end
+end
+
+function M.blame_to_table(blame)
+	local lines = {}
+	print("+++++++++++++++++++++++++++++++++++++++++++++++++")
+	for line in blame:gmatch("[^\n]+") do
+		print(line)
+		table.insert(lines, line)
+	end
+	print("+++++++++++++++++++++++++++++++++++++++++++++++++")
+	local numbers = {}
+	-- split lines by firest 8 spaces
+	for _, line in ipairs(lines) do
+		-- column number is 9-th on my machine
+		table.insert(numbers, tonumber((vim.fn.split(line, " ", true)[9]):match("%d+")))
+	end
+	return numbers
 end
 
 function M.listen_to_events()
 	for _, event in ipairs(events) do
-		vim.cmd("autocmd " .. event .. " * lua require('find-next').print_hi()")
+		vim.cmd("autocmd " .. event .. " * lua require('find-next').update_lines()")
 	end
 end
 
@@ -62,20 +91,52 @@ function M.move_pointer(line_number)
 	vim.fn.cursor({ line_number, 0 })
 end
 
-function M.setup()
-	-- print(M.get_branch_name())
-	print("----------")
+-- convert blame output to a table
+function M.split_by_groups(numbers)
+	local prev_number = 0
+	local first_in_sequence = {}
 
+	for _, number in ipairs(numbers) do
+		print("--" .. number)
+	end
+
+	for _, number in ipairs(numbers) do
+		print(number)
+		if number ~= prev_number + 1 then
+			print("Adding..")
+			table.insert(first_in_sequence, number)
+		end
+		prev_number = number
+	end
+
+	return first_in_sequence
+end
+
+function M.setup()
+	-- log message to nvim console
+	-- print("Find Next loaded")
+	-- print(M.get_branch_name())
+	-- print("----------")
+	--
 	M.listen_to_events()
 	-- local changes = M.get_text("git blame ./lua/find-next/init.lua")
 	-- print(M.filter_lines(changes))
 
 	vim.keymap.set("n", "gBa", function()
-		print(M.get_blame())
-		M.move_pointer(7)
+		M.update_lines()
 	end)
-	vim.keymap.set("n", "gBw", function()
-		print(vim.fn.line("."))
+	vim.keymap.set("n", "gBc", function()
+		local next_change_block_line = vim.fn.line(".")
+
+		for _, value in ipairs(M.split_by_groups(changedLines)) do
+			print(value, next_change_block_line)
+
+			if value > next_change_block_line then
+				print("Moving to line " .. value)
+				M.move_pointer(value)
+				break
+			end
+		end
 	end)
 	-- print(M.get_blame())
 end
